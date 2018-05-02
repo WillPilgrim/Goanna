fda77af5300f5f72412e630f8df6c5056179d497
Sean Turner <tabsat@rwwa.com.au>
2018-05-02 15:07:12 +0800

    Base level webpack and npm

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // index.js
+import moment from 'moment';
+
 console.log("Hello from JavaScript!");
 console.log(moment().startOf('day').fromNow());
+
+let name = "Sean", time = "tomorrow";
+console.log(`Hello ${name}, how are you ${time}?`);
